bc11af944080d91ee66d7f48c1f09ee41937d380
Sunesh <Sunesh.Padinhantan@ust-global.com>
2018-01-15 18:04:28 +0530

    added sample data

--- ICEWebAPI/Controllers/QueueController.cs
+++ ICEWebAPI/Controllers/QueueController.cs
@@ -25,23 +25,28 @@ namespace ICEWebAPI.Controllers
             var result = new List<Account>();
             try
             {
-                TokenProvider tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(ICEConfig.keyName, ICEConfig.accessKey);
-                MessagingFactory messagingFactory = MessagingFactory.Create(ICEConfig.baseAddress, tokenProvider);
-                var receiver = await messagingFactory.CreateMessageReceiverAsync(ICEConfig.queueName, ReceiveMode.ReceiveAndDelete);
+                //TokenProvider tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(ICEConfig.keyName, ICEConfig.accessKey);
+                //MessagingFactory messagingFactory = MessagingFactory.Create(ICEConfig.baseAddress, tokenProvider);
+                //var receiver = await messagingFactory.CreateMessageReceiverAsync(ICEConfig.queueName, ReceiveMode.ReceiveAndDelete);
 
-                while (true)
+                //while (true)
+                //{
+                //    var msg = await receiver.ReceiveAsync(TimeSpan.Zero);
+                //    if (msg != null)
+                //    {
+                //        var serializer = new XmlSerializer(typeof(Account));
+                //        var objresult = (Account)serializer.Deserialize(new StreamReader(msg.GetBody<Stream>()));
+                //        result.Add(objresult);
+                //    }
+                //    else
+                //    {
+                //        break;
+                //    }
+                //}
+
+                for (int i = 0; i < 10; i++)
                 {
-                    var msg = await receiver.ReceiveAsync(TimeSpan.Zero);
-                    if (msg != null)
-                    {
-                        var serializer = new XmlSerializer(typeof(Account));
-                        var objresult = (Account)serializer.Deserialize(new StreamReader(msg.GetBody<Stream>()));
-                        result.Add(objresult);
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    result.Add(new Account() { Name = "Test Name ", Store_Type_Desc__c = "sample description", Description = "Sample Simple description" });
                 }
             }
             catch (Exception)
