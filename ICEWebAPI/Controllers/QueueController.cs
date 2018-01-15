25dbcc5b7217ef5c433ea2fef1e31768c11c1183
Sunesh <Sunesh.Padinhantan@ust-global.com>
2018-01-15 17:50:11 +0530

    restructred api method

--- ICEWebAPI/Controllers/QueueController.cs
+++ ICEWebAPI/Controllers/QueueController.cs
@@ -15,12 +15,12 @@ namespace ICEWebAPI.Controllers
     {
         [HttpGet]
         [Route("~/readqueue")]
-        public async Task<IEnumerable<Account>> Get()
+        public async Task<List<Account>> Get()
         {
             return await Queue();
         }
 
-        public async Task<IEnumerable<Account>> Queue()
+        public async Task<List<Account>> Queue()
         {
             var result = new List<Account>();
             try
