acd6eeec6792b5629cc64f3eecf4dd9d4e50972f
Sunesh <Sunesh.Padinhantan@ust-global.com>
2018-01-15 18:28:52 +0530

    method changed

--- ICEWebAPI/Controllers/QueueController.cs
+++ ICEWebAPI/Controllers/QueueController.cs
@@ -25,29 +25,33 @@ namespace ICEWebAPI.Controllers
             var result = new List<AccountNew>();
             try
             {
-                //TokenProvider tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(ICEConfig.keyName, ICEConfig.accessKey);
-                //MessagingFactory messagingFactory = MessagingFactory.Create(ICEConfig.baseAddress, tokenProvider);
-                //var receiver = await messagingFactory.CreateMessageReceiverAsync(ICEConfig.queueName, ReceiveMode.ReceiveAndDelete);
+                TokenProvider tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(ICEConfig.keyName, ICEConfig.accessKey);
+                MessagingFactory messagingFactory = MessagingFactory.Create(ICEConfig.baseAddress, tokenProvider);
+                var receiver = await messagingFactory.CreateMessageReceiverAsync(ICEConfig.queueName, ReceiveMode.ReceiveAndDelete);
 
-                //while (true)
-                //{
-                //    var msg = await receiver.ReceiveAsync(TimeSpan.Zero);
-                //    if (msg != null)
-                //    {
-                //        var serializer = new XmlSerializer(typeof(Account));
-                //        var objresult = (Account)serializer.Deserialize(new StreamReader(msg.GetBody<Stream>()));
-                //        result.Add(objresult);
-                //    }
-                //    else
-                //    {
-                //        break;
-                //    }
-                //}
-
-                for (int i = 0; i < 10; i++)
+                while (true)
                 {
-                    result.Add(new AccountNew() { Name = "Test Name ", Store_Type_Desc__c = "sample description", Description = "Sample Simple description" });
+                    var msg = await receiver.ReceiveAsync(TimeSpan.Zero);
+                    if (msg != null)
+                    {
+                        var serializer = new XmlSerializer(typeof(Account));
+                        var objresult = (Account)serializer.Deserialize(new StreamReader(msg.GetBody<Stream>()));
+                        if (objresult!=null)
+                        {
+                            result.Add(new AccountNew()
+                            {
+                                Name= objresult.Name,
+                                Phone = objresult.Phone,                                
+                            });
+                        }
+                       
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
+               
             }
             catch (Exception)
             {
@@ -61,7 +65,20 @@ namespace ICEWebAPI.Controllers
     public class AccountNew
     {
         public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public string SicDesc { get; set; }
+        public string Reference_Number__c { get; set; }
+        public string Fax_Number__c { get; set; }
+        public string CGO_Email__c { get; set; }
         public string Store_Type_Desc__c { get; set; }
-        public string Description { get; set; }
+        public string Opening_Hours_Main__c { get; set; }
+        public string Store_Manager__c { get; set; }
+        public string Store_Manager_Email__c { get; set; }
+        public string Area_Manager_Email__c { get; set; }
+        public string Area_Name__c { get; set; }
+        public string Store_Manager_Contact__c { get; set; }
+        public string Area_Manager_Contact__c { get; set; }
     }
 }
+
